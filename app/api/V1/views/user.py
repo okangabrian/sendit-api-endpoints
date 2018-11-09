d78e3059c813e94057c5168348084063baec059f
okangabrian <brianokanga7@gmail.com>
2018-11-09 20:43:13 +0300

    [Feature-user-login-#161774604)] create user login

--- app/api/V1/views/user.py
+++ app/api/V1/views/user.py
@@ -18,3 +18,24 @@ class PostUser(Resource):
         user = User(username, password)
         users.append(user)
         return {"message": "successful"}, 201
+
+
+class Login(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("username", type=str, required=True)
+    parser.add_argument("password", type=str, required=True)
+
+    def post(self):
+        data = Login.parser.parse_args()
+
+        username = data["username"]
+        password = data["password"]
+
+        user = User().get_user_by_name(username)
+
+        if not user:
+            return {'message': 'user not found'}, 404
+
+        if not check_password_hash(user.password, password):
+            return {'message': 'incorrect password'}, 401
+        return {"message": 'login successful'}
