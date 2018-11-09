10fc6a0c95b92ff4ee61d8e12e0f0e217838206d
okangabrian <brianokanga7@gmail.com>
2018-11-09 13:26:27 +0300

    [Feature-add-new-user-#161774612]add new user

--- app/api/V1/views/user.py
+++ app/api/V1/views/user.py
@@ -1,1 +1,19 @@
 from flask_restful import Resource, reqparse
+
+from ..models.models import Parcel, User, users, parcels
+
+
+class PostUser(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument("username", type=str, required=True)
+    parser.add_argument("password", type=str, required=True)
+
+    def post(self):
+        """ method create user """
+        data = PostUser.parser.parse_args()
+        username = data["username"]
+        password = data["password"]
+        user = User(username, password)
+        users.append(user)
+        return {"message": "successful"}, 201
